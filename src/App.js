7858aa2e258d9b514fe9cc75bfff0d6290bf5e98
Cisco31 <francisco.m.0331@gmail.com>
2022-02-20 20:05:09 -0700

    creacion y descarga de memes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import html2canvas from 'html2canvas';
 
 function App() {
+  const [linea1, setLinea1] = useState('');
+  const [linea2, setLinea2] = useState('');
+  const [imagen, setImagen] = useState('');
+
+  const onChangeLinea1 = function(evento){
+    setLinea1(evento.target.value);
+  }
+
+  const onChangeLinea2 = function(evento){
+    setLinea2(evento.target.value);
+  }
+
+  const onChangeImagen = function(evento){
+    setImagen(evento.target.value);
+  }
+
+  const onclickExportar = function(evento){
+    alert("Exportar");
+    html2canvas(document.querySelector("#meme")).then(canvas => {
+      var img    = canvas.toDataURL("image/png");
+      var link = document.createElement('a');
+      link.download = 'meme.png';
+      link.href = img;
+      link.click();
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select onChange={onChangeImagen}>
+        <option value="bob_esponja">Bob Esponja</option>
+        <option value="bob">Bob</option>
+        <option value="homero">Homero</option>
+        <option value="negro">Negro</option>
+        <option value="golpeandome">Golpeandome</option>
+        <option value="pikachu">Pikachu</option>
+        <option value="puerco">Puerco</option>
+        <option value="sid">Sid</option>
+        <option value="son_expertos">Son expertos</option>
+        <option value="sabatico">Sabatico</option>
+      </select><br/>
+      <input onChange={onChangeLinea1} type="text" placeholder='Linea 1'/> <br/>
+      <input onChange={onChangeLinea2} type="text" placeholder='Linea 2'/> <br/>
+      <button onClick={onclickExportar}>Exportar</button>
+
+      <div className='meme' id="meme">
+        <span>{linea1}</span><br/>
+        <span>{linea2}</span>
+        
+        
+        <img src={"/img/" + imagen + ".jpg"} />
+      </div>
+
     </div>
   );
 }
